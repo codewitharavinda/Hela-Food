491c992f334d67335242cec0b54b6c6ca940ebf5
Aravinda Amarasingha <supunaravinda2004@gmail.com>
2025-11-26 14:30:03 +0530

    first commit

---
+++ components/js/app.js
@@ -0,0 +1,35 @@
+fetch("components/nav&footer/nav.html")
+.then(res => res.text())
+.then(data => {
+    document.getElementById("nav").innerHTML = data;
+});
+
+fetch("components/nav&footer/footer.html")
+.then(res => res.text())
+.then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
+showHomeNav();
+function showShopNav() {
+    fetch("components/shop/shop.html")
+    .then(res => res.text())
+    .then(data => {
+    document.getElementById("root").innerHTML = data;
+});
+}
+
+function showHomeNav(){
+    fetch("components/home/home.html")
+    .then(res => res.text())
+    .then(data => {
+    document.getElementById("root").innerHTML = data;
+    });
+}
+
+function showAboutNav(){
+    fetch("components/about/about.html")
+    .then(res => res.text())
+    .then(data => {
+    document.getElementById("root").innerHTML = data;
+    });
+}
